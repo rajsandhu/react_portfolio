af41546fe2716911b585a0ba66a7ac8835217739
rajsandhu <rajsandhu@gmail.com>
2019-10-26 20:13:38 +0200

    in Header.js, abstracted class Header's jsx to a functional component

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 
+const HeaderTitle = () => {
+    return (
+        <h1>This is my Header</h1>
+    )
+}
+
 class Header extends Component {
     render() {
         return (
-            <h1>This is my Header</h1>
+            <HeaderTitle />
         )
     }
 }
